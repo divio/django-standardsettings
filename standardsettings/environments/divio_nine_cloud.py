90a87c1c6e5885c791bc6d2d3bb105c47827350c
Stefan Foulis <stefan@foulis.ch>
2013-09-26 11:03:55 +0200

    fixes bug in redis log path for nine deployment

--- standardsettings/environments/divio_nine_cloud.py
+++ standardsettings/environments/divio_nine_cloud.py
@@ -9,7 +9,7 @@ def apply_settings(settings):
     settings.DATA_ROOT = '/home/%(username)s/upload/' % server_cfg
     settings.MEDIA_ROOT = '/home/%(username)s/upload/media/' % server_cfg
     settings.STATIC_ROOT = '/home/%(username)s/static/' % server_cfg
-    settings.LOG_ROOT = '/home/%(username)s/log/'
+    settings.LOG_ROOT = '/home/%(username)s/log/' % server_cfg
     settings.SOCKET_ROOT = '/home/%(username)s/tmp/' % server_cfg
     settings.REDIS_SOCKET = '/home/%(username)s/tmp/%(username)s_%(site)s_redis.sock' % server_cfg
     settings.ALLOWED_HOSTS = env("ALLOWED_HOSTS", ['%(username)s.divio.ch' % server_cfg])
